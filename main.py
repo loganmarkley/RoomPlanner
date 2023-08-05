7585e7e901832dd15f5fc35121cac7e3de517a75
Logan Markley <114544357+loganmarkley@users.noreply.github.com>
2023-08-04 19:37:23 -0500

    Added furniture panel, button text, and more

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Author: Logan Markley
 # Last Updated: 8/4/2023
 # Version: Not Finished
-# Latest Addition: Added wall dimensions and a show grid button!
+# Latest Addition: Added furniture panel and text to the buttons, and some other small changes
 # Date Started: 8/2/2023
 # Desc: Using Pygame and maybe Pickle, create a fully functioning room planner
 #       where you can drag and drop items, customize dimensions, etc.
@@ -21,43 +21,25 @@ def draw_line(vertex1, vertex2) -> None:  # this static function allows for easy
     # to the center of our rectangle, so we need to offset the position vector by half of the radius
 
 
-def draw_grid() -> None:
-    for i in range(0, 80):  # draws horizontal lines and then vertical lines with 15 pixel gaps
-        pygame.draw.line(screen, (225, 225, 225), (0, i * 15), (SCREEN_WIDTH, i * 15))
-        pygame.draw.line(screen, (225, 225, 225), (i * 15, 0), (i * 15, SCREEN_HEIGHT))
-
-
-def draw_add_vertex_button(rect) -> None:       #makes a green button in bottom right corner
-    pygame.draw.rect(screen, (100, 200, 100), rect)
-
-
-def draw_minus_vertex_button(rect) -> None:     # makes a red button in bottom right corner
-    pygame.draw.rect(screen, (200, 100, 100), rect)
-
-
-def draw_show_grid_button(rect) -> None:     # makes a red button in bottom right corner
-    pygame.draw.rect(screen, (150, 150, 150), rect)
-
-
-def calculate_distance(vertex1, vertex2) -> int:    # returns the distance between two vertices in "inches" (5px = 1in)
+def calculate_distance(vertex1, vertex2) -> int:  # returns the distance between two vertices in "inches"
     x_difference = vertex1.rect.x - vertex2.rect.x
     y_difference = vertex1.rect.y - vertex2.rect.y
     hypotenuse = sqrt(x_difference ** 2 + y_difference ** 2)
-    return int(hypotenuse // 3)
+    return int(hypotenuse // 4)  # (4px = 1in)
 
 
 def calculate_halfway_point(vertex1, vertex2) -> tuple:
-    x_pos = (vertex1.rect.x + vertex2.rect.x) // 2
-    y_pos = (vertex1.rect.y + vertex2.rect.y) // 2
+    x_pos = (vertex1.rect.centerx + vertex2.rect.centerx) // 2
+    y_pos = (vertex1.rect.centery + vertex2.rect.centery) // 2
     return x_pos, y_pos
 
 
 class Room:
     def __init__(self):
-        self.vertex1 = Vertex(400, 300)
-        self.vertex2 = Vertex(800, 300)
-        self.vertex3 = Vertex(800, 600)
-        self.vertex4 = Vertex(400, 600)
+        self.vertex1 = Vertex(700, 300)
+        self.vertex2 = Vertex(1100, 300)
+        self.vertex3 = Vertex(1100, 600)
+        self.vertex4 = Vertex(700, 600)
 
         self.wall_vertices = [self.vertex1, self.vertex2, self.vertex3, self.vertex4]
 
@@ -87,16 +69,17 @@ class Room:
             self.wall_vertices[i].draw_vertex()
 
     def draw_wall_dimensions(self) -> None:
+        f = pygame.font.Font('Fonts/MADEVoyager.otf', 16)
         for i in range(0, len(self.wall_vertices)):
             if i == len(self.wall_vertices) - 1:
                 distance = calculate_distance(self.wall_vertices[i], self.wall_vertices[0])
                 halfway_pt = calculate_halfway_point(self.wall_vertices[i], self.wall_vertices[0])
             else:
-                distance = calculate_distance(self.wall_vertices[i], self.wall_vertices[i+1])
+                distance = calculate_distance(self.wall_vertices[i], self.wall_vertices[i + 1])
                 halfway_pt = calculate_halfway_point(self.wall_vertices[i], self.wall_vertices[i + 1])
             dimension = str(distance) + '"'
-            text_surface = font.render(dimension, True, (100, 100, 100))
-            screen.blit(text_surface, (halfway_pt[0], halfway_pt[1]))
+            text_surface = f.render(dimension, True, (100, 100, 100))
+            screen.blit(text_surface, (halfway_pt[0] + 5, halfway_pt[1] - 20))  # arbitrary styling numbers
 
 
 class Vertex:
@@ -106,6 +89,58 @@ class Vertex:
 
     def draw_vertex(self) -> None:
         pygame.draw.rect(screen, (90, 90, 90), self.rect, 0, VERTEX_RADIUS)
+
+
+class UserInterface:
+    def __init__(self):
+        self.FURNITURE_PANEL_WIDTH = 400
+        self.btn_add_vertex_rect = pygame.Rect(SCREEN_WIDTH - 75, SCREEN_HEIGHT - 75, 50, 50)
+        self.btn_minus_vertex_rect = pygame.Rect(SCREEN_WIDTH - 140, SCREEN_HEIGHT - 75, 50, 50)
+        self.btn_show_grid_rect = pygame.Rect(30 + self.FURNITURE_PANEL_WIDTH, SCREEN_HEIGHT - 75, 50, 50)
+
+    def draw_user_interface(self):
+        self.draw_furniture_panel()
+        pygame.draw.rect(screen, (100, 200, 100), self.btn_add_vertex_rect, 0, 10)
+        pygame.draw.rect(screen, (200, 100, 100), self.btn_minus_vertex_rect, 0, 10)
+        pygame.draw.rect(screen, (150, 150, 150), self.btn_show_grid_rect, 0, 10)
+
+        f = pygame.font.Font('Fonts/MADEVoyager.otf', 23)
+        text_surface = f.render('Grid', True, (50, 50, 50))
+        screen.blit(text_surface, (self.FURNITURE_PANEL_WIDTH + 33, SCREEN_HEIGHT - 69))
+        text_surface = f.render('Corners:', True, (100, 100, 100))
+        screen.blit(text_surface, (SCREEN_WIDTH - 240, SCREEN_HEIGHT - 70))
+
+        f = pygame.font.Font('Fonts/MADEVoyager.otf', 80)
+        text_surface = f.render('-', True, (80, 80, 80))
+        screen.blit(text_surface, (SCREEN_WIDTH - 127, SCREEN_HEIGHT - 114))  # minus sign on button
+        text_surface = f.render('+', True, (80, 80, 80))
+        screen.blit(text_surface, (SCREEN_WIDTH - 66, SCREEN_HEIGHT - 110))  # plus sign on button
+
+    def draw_grid(self, draw_grid_bool):
+        if draw_grid_bool:
+            for i in range(0, 76):  # draws horizontal lines and then vertical lines with 15 pixel gaps
+                pygame.draw.line(screen, (225, 225, 225), (self.FURNITURE_PANEL_WIDTH, i * 15), (SCREEN_WIDTH, i * 15))
+                pygame.draw.line(screen, (225, 225, 225), (self.FURNITURE_PANEL_WIDTH + i * 15, 0),
+                                 (self.FURNITURE_PANEL_WIDTH + i * 15, SCREEN_HEIGHT))
+
+    def draw_furniture_panel(self):
+        rect = pygame.Rect(0, 0, self.FURNITURE_PANEL_WIDTH, SCREEN_HEIGHT)  # background gray of the panel
+        pygame.draw.rect(screen, (230, 230, 230), rect)
+        rect = pygame.Rect(self.FURNITURE_PANEL_WIDTH, 0, 2, SCREEN_HEIGHT)  # vertical line isolates panel
+        pygame.draw.rect(screen, (120, 120, 120), rect)
+        rect = pygame.Rect(self.FURNITURE_PANEL_WIDTH // 2, 55, 2, SCREEN_HEIGHT)  # vert line separates panel in half
+        pygame.draw.rect(screen, (180, 180, 180), rect)
+        rect = pygame.Rect(0, 55, self.FURNITURE_PANEL_WIDTH, 2)  # top horizontal line to separate text
+        pygame.draw.rect(screen, (120, 120, 120), rect)
+
+        f = pygame.font.Font('Fonts/MADEVoyager.otf', 35)
+        text_surface = f.render('Furniture', True, (100, 100, 100))
+        screen.blit(text_surface, (130, 5))  # arbitrary positioning numbers
+
+        quarter_height = (SCREEN_HEIGHT - 55) // 4  # 55px is how far the separating line is from the top
+        for i in range(1, 4):  # draws 3 lines to make 8 furniture piece sections
+            rect = pygame.Rect(0, 55 + quarter_height * i, self.FURNITURE_PANEL_WIDTH, 2)
+            pygame.draw.rect(screen, (180, 180, 180), rect)
 
 
 # class CameraGroup(pygame.sprite.Group):
@@ -142,7 +177,7 @@ pygame.display.set_caption('Room Planner')
 
 # camera_group = pygame.sprite.Group()
 
-SCREEN_WIDTH = 1200
+SCREEN_WIDTH = 1500
 SCREEN_HEIGHT = 950
 SCREEN_BACKGROUND_COLOR = (240, 240, 230)
 VERTEX_RADIUS = 7
@@ -151,16 +186,12 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 clock = pygame.time.Clock()
 
-font = pygame.font.Font('Fonts/MilkyAgain.ttf', 18)
-
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
 
 room = Room()
+ui = UserInterface()
 active_vertex_index = -1
-add_vertex_rect = pygame.Rect(SCREEN_WIDTH - 75, SCREEN_HEIGHT - 75, 50, 50)
-minus_vertex_rect = pygame.Rect(SCREEN_WIDTH - 150, SCREEN_HEIGHT - 75, 50, 50)
-show_grid_rect = pygame.Rect(20, 20, 50, 50)
 show_grid_bool = True
 while True:
     for event in pygame.event.get():
@@ -173,11 +204,11 @@ while True:
                     if vertex.rect.collidepoint(event.pos):
                         active_vertex_index = num
 
-                if add_vertex_rect.collidepoint(event.pos):
+                if ui.btn_add_vertex_rect.collidepoint(event.pos):
                     room.add_vertex()
-                if minus_vertex_rect.collidepoint(event.pos):
+                if ui.btn_minus_vertex_rect.collidepoint(event.pos):
                     room.minus_vertex()
-                if show_grid_rect.collidepoint(event.pos):
+                if ui.btn_show_grid_rect.collidepoint(event.pos):
                     show_grid_bool = not show_grid_bool
 
         if event.type == pygame.MOUSEBUTTONUP:
@@ -192,14 +223,11 @@ while True:
 
     screen.fill(SCREEN_BACKGROUND_COLOR)
 
-    if show_grid_bool:
-        draw_grid()
+    ui.draw_grid(show_grid_bool)
     room.draw_walls()
     room.draw_vertices()
-    draw_add_vertex_button(add_vertex_rect)
-    draw_minus_vertex_button(minus_vertex_rect)
-    draw_show_grid_button(show_grid_rect)
     room.draw_wall_dimensions()
+    ui.draw_user_interface()
 
     pygame.display.update()
-    clock.tick(60)  # this while loop will never run more than 60 fps
+    clock.tick(75)  # this while loop will never run more than 75 fps
